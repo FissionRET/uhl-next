ef15442cea60b3bfd847c9b195f60d1e9308e616
Zord <quangsang.red@gmail.com>
2024-08-24 00:15:55 +0700

    login form UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,18 @@
 import Particles from "@/components/magicui/particles";
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 export default function Login() {
   const { theme } = useTheme();
   const [color, setColor] = useState("#ffffff");
@@ -20,8 +32,54 @@ export default function Login() {
           color={color}
           refresh
         />
-        <main className="flex flex-col items-center justify-between p-24">
-            test
+        <main className="flex flex-col items-center justify-between p-24 z-10 relative">
+          <Card className="absolute w-[350px] bg-opacity-25 dark:bg-opacity-25">
+            <CardHeader>
+              <CardTitle>Đăng Nhập</CardTitle>
+              <CardDescription>
+                Sử dụng mã sinh viên và mật khẩu của nhà trường
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Mã sinh viên</Label>
+                    <Input
+                      className="bg-opacity-25 dark:bg-opacity-25"
+                      id="msv"
+                      name="msv"
+                      placeholder=""
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Mật Khẩu</Label>
+                    <Input
+                      className="bg-opacity-25 dark:bg-opacity-25"
+                      id="password"
+                      name="password"
+                      placeholder=""
+                    />
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <Checkbox
+                      className="bg-opacity-25 dark:bg-opacity-25"
+                      id="terms"
+                    />
+                    <Label htmlFor="terms">Nhớ mật khẩu trong hệ thống</Label>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-end px-5">
+              <Button
+                className="bg-opacity-25 dark:bg-opacity-25"
+                variant="outline"
+              >
+                Đăng Nhập
+              </Button>
+            </CardFooter>
+          </Card>
         </main>
       </div>
     </>
