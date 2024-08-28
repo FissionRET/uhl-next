acfae72159941e1893a4b8b091bcc6939a2a06cd
ChinoTheGod <meodihia756@gmail.com>
2024-08-28 12:07:11 +0700

    Responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,45 @@
 import BlurIn from "@/components/magicui/blur-in";
-import { ModeToggle } from './mode-toggle';
+import { ModeToggle } from "./mode-toggle";
 
 export default function Navbar() {
     return (
         <section className="py-12">
-            <div className="container">
-                <nav className="hidden justify-between lg:flex">
+            <div className="container mx-auto px-4">
+                {/* Desktop Navbar */}
+                <nav className="hidden justify-between items-center lg:flex">
                     <div className="flex items-center gap-6">
                         <div className="flex items-center gap-2">
-                            <a href="/uhl-next">
-                                <BlurIn word="UHL Next" className="text-xl font-bold subpixel-antialiased"/>
+                            <a href="/">
+                                <BlurIn
+                                    word="UHL Next"
+                                    className="text-xl font-bold subpixel-antialiased"
+                                />
                             </a>
                         </div>
 
                         <div className="flex items-center">
-                            <ModeToggle/>
+                            <ModeToggle />
                         </div>
                     </div>
+                </nav>
+
+                {/* Mobile Navbar */}
+                <nav className="flex justify-between items-center lg:hidden">
+                    {/* Logo */}
+                    <div className="flex items-center gap-6">
+                        <div className="flex items-center gap-2">
+                            <a
+                                href="/"
+                                className="text-xl font-bold subpixel-antialiased"
+                            >
+                                UHL Next
+                            </a>
+                        </div>
+
+                        <div className="flex items-center">
+                            <ModeToggle />
+                        </div>
+                    </div>                    
                 </nav>
             </div>
         </section>
